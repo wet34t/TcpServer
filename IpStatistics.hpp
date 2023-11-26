d3255c67f8de397713a104d98fcc34880f388a7f
Dylan Liu <lly819@hotmail.com>
2023-11-26 15:35:19 -0800

    Add files via upload (#2)

--- IpStatistics.hpp
+++ IpStatistics.hpp
@@ -7,64 +7,114 @@
 struct IpStatistics_t {
 private:
 time_t start_;
+// Simple map of ip (strings) to tuple of info - total connections, successful connections and connection errors
 std::map<std::string, std::tuple<unsigned, unsigned, unsigned>> addressesInfo_;
-mutable std::mutex m;
+mutable std::mutex m; // Simple mutex guarding against access to addressesInfo_;
 
 public:
+    /*
+    * struct IpStatistics_t - Constructor
+    * Simple statistics struct to hold some stats based on IP
+    */
     IpStatistics_t() :
     start_(time(0))
     {
     }
 
+    /*
+    * To clear all stats
+    */
     void Reset() {
         std::lock_guard<std::mutex> l(m);
         addressesInfo_.clear();
+        start_ = time(0);
     }
 
+    /*
+    * Register a new connection from IP
+    *
+    * param - std::string ipAddress: IP in string
+    */
     void RegisterConnection(std::string &ipAddress) {
         std::lock_guard<std::mutex> l(m);
         std::get<0>(addressesInfo_[ipAddress])++;
     }
 
+    /*
+    * Register a new successful connection from IP
+    *
+    * param - std::string ipAddress: IP in string
+    */
     void RegisterSuccess(std::string &ipAddress) {
         std::lock_guard<std::mutex> l(m);
         std::get<1>(addressesInfo_[ipAddress])++;
     }
 
+    /*
+    * Register a new connection error from IP
+    *
+    * param - std::string ipAddress: IP in string
+    */
     void RegisterErrors(std::string &ipAddress) {
         std::lock_guard<std::mutex> l(m);
         std::get<2>(addressesInfo_[ipAddress])++;
     }
 
+    /*
+    * Get total connections from IP
+    *
+    * param - std::string ipAddress: IP in string
+    *
+    * return unsigned - number of connections by this IP
+    */
     unsigned GetConnection(const std::string &ipAddress) const {
         std::lock_guard<std::mutex> l(m);
         try {
             return std::get<0>(addressesInfo_.at(ipAddress));
-        } catch (std::out_of_range) {
+        } catch (std::out_of_range) { // Doesn't exist
             return 0;
         }
     }
 
+    /*
+    * Get total succssful connections from IP
+    *
+    * param - std::string ipAddress: IP in string
+    *
+    * return unsigned - number of succss by this IP
+    */
     unsigned GetSuccess(const std::string &ipAddress) const {
         std::lock_guard<std::mutex> l(m);
         try {
             return std::get<1>(addressesInfo_.at(ipAddress));
-        } catch (std::out_of_range) {
+        } catch (std::out_of_range) { // Doesn't exist
             return 0;
         }
     }
 
+    /*
+    * Get total connections errors from IP
+    *
+    * param - std::string ipAddress: IP in string
+    *
+    * return unsigned - number of errors by this IP
+    */
     unsigned GetErrors(const std::string &ipAddress) const {
         std::lock_guard<std::mutex> l(m);
         try {
             return std::get<2>(addressesInfo_.at(ipAddress));
-        } catch (std::out_of_range) {
+        } catch (std::out_of_range) { // Doesn't exist
             return 0;
         }
     }
 
-    unsigned GetTimeAlive() const {
-        return time(0) - start_;
+    /*
+    * Get start time
+    *
+    * return time_t - when this IpStatistics_t got created
+    */
+    time_t GetStartTime() const {
+        return start_;
     }
 };
 
--- TcpConnection.cpp
+++ TcpConnection.cpp
@@ -1,20 +1,18 @@
 #include "TcpConnection.hpp"
+#include <iomanip>
 
 using namespace boost;
 
 std::string TcpConnection::getHeaderType(unsigned *headerType, system::error_code &err) {
-    if (headerType[0] == 225 && headerType[1] == 16) {
-        std::cout << "Header is Hello" << std::endl;
-        return "Hello";     // Do all messages need a Hello first?
-    } else if (headerType[0] == 218 && headerType[1] == 122) {
-        std::cout << "Header is Data" << std::endl;
-        return "Data";
-    } else if (headerType[0] == 11 && headerType[1] == 30) {
-        std::cout << "Header is Goodbye" << std::endl;
+    if (headerType[0] == HELLO1 && headerType[1] == HELLO2) {
+        return HELLO; // Make no assumptions whether all messages need to start with Hello
+    } else if (headerType[0] == DATA1 && headerType[1] == DATA2) {
+        return DATA;
+    } else if (headerType[0] == GOODBYE1 && headerType[1] == GOODBYE2) {
         byeReceived_ = true;
-        return "Goodbye";   // Presumably ending connection
-    } else {
-        err = boost::system::errc::make_error_code(boost::system::errc::bad_message);
+        return GOODBYE; // Assumes when Goodbye is received, stop receiving messages
+    } else { // Unsupported header
+        err = system::errc::make_error_code(system::errc::bad_message);
         return "";
     }
 }
@@ -22,18 +20,15 @@ std::string TcpConnection::getHeaderType(unsigned *headerType, system::error_cod
 void TcpConnection::readBytes(size_t len, system::error_code &err) {
     std::vector<unsigned char> buff(len);
     sock_.read_some(asio::buffer(buff.data(), len), err);
+    // Potentially can parse data as needed, i.e parse(buff);
 }
 
-void TcpConnection::readBytesToHex(size_t len, unsigned *result, system::error_code &err) {
+void TcpConnection::readBytesToHex(size_t len, unsigned *result, system::error_code &err) { // This is to dec technically
     std::vector<unsigned char> buff(len);
     sock_.read_some(asio::buffer(buff.data(), len), err);
 
     for (int i=0; i<len; i++) {
-        result[i] = (unsigned)buff[i];  // To dec
-        if (result[i] > 255) {  // Overflow
-            err = boost::system::errc::make_error_code(boost::system::errc::bad_message);
-            return;
-        }
+        result[i] = (unsigned)buff[i]; // cast it to unsigned
     }
 }
 
@@ -44,8 +39,11 @@ bool TcpConnection::waitForBytes(size_t len) {
     if (err) return false;
     while (bytes < len) {
         size_t next = sock_.available(err);
-        if (err || (next == bytes && time(0) - s > TIMEOUT)) return false;
-        else if (next != bytes) {
+        if (err || (next == bytes && time(0) - s > TIMEOUT)) {
+            // Been waiting too long without getting any data at all
+            return false;
+        } else if (next != bytes) {
+            // Got some data, so reset timer
             bytes = next;
             s = time(0);
         }
@@ -53,87 +51,114 @@ bool TcpConnection::waitForBytes(size_t len) {
     return true;
 }
 
-void TcpConnection::processBody(size_t bodyLen) {
+std::string TcpConnection::processBody(size_t bodyLen) {
     system::error_code err;
-    if (!waitForBytes(bodyLen)) {
+    // Only process the first 4 bytes or entire body whichever is less
+    int hexes = std::min(DATAPRINT, (int)bodyLen);
+    if (!waitForBytes(hexes)) {
         handleError("Error waiting for body message");
-        return;
+        return "";
     }
-    int hexes = std::min(4, (int)bodyLen);
     unsigned bodyHexes[hexes];
     readBytesToHex(hexes, bodyHexes, err);
     if (err) {
         handleError("Error occured reading body message");
+        return "";
     }
+
+    std::stringstream ret; 
+    for (int i=0; i<hexes; i++) {
+        // This just converts it to hex format as required
+        ret << "0x" << std::hex << std::setfill('0') << std::setw(2) << bodyHexes[i];
+        if (i < hexes-1) {
+            ret << " ";
+        } 
+    }
+
+    // Process the rest of the body, max MAXBATCHSIZE per batch 
     bodyLen -= hexes;
-    std::cout << "Got header type " << bodyHexes[0] << " " << bodyHexes[1] << " " << bodyHexes[2] << " " << bodyHexes[3] << std::endl; // TODO better print
-    // << std::hex << std::setfill ('0') << std::setw(sizeof(your_type)*2) 
     int len = std::min(MAXBATCHSIZE, (int)bodyLen);
     while (len <= bodyLen) {
         if (!len) break;
+        if (!waitForBytes(len)) {
+            handleError("Error waiting for body message");
+            return "";
+        }
         readBytes(len, err);
         if (err) {
             handleError("Error occured reading rest of the body messages");
-            return;
+            return "";
         }
         bodyLen -= len;
         len = std::min(MAXBATCHSIZE, (int)bodyLen);
     }
+    return ret.str();
 }
 
-size_t TcpConnection::processHeader() {     
+std::string TcpConnection::processHeader(size_t *len) {     
     system::error_code err;
+    // Process the type which is first 2 bytes
     if (!waitForBytes(TYPESIZE)) {
         handleError("Error waiting to read header type");
-        return 0;
+        return "";
     }
-
     unsigned headerType[TYPESIZE];
     readBytesToHex(TYPESIZE, headerType, err);
-    std::cout << "Got header type " << headerType[0] << " " << headerType[1] << std::endl;
     if (err) {
         handleError("Error occured reading header type");
-        return 0;
+        return "";
     }
-    std::string result = getHeaderType(headerType, err);
+    std::string ret = "[";
+    ret += getHeaderType(headerType, err);
+    ret += "] [";
     if (err) {
         handleError("Error not supported header type");
-        return 0;
+        return "";
     }
 
+    // Process the Length, which is next 4 bytes
     if (!waitForBytes(LENSIZE)) {
         handleError("Error waiting to read header length");
-        return 0;
+        return "";
     }
-
     unsigned headerLen[LENSIZE];
     readBytesToHex(LENSIZE, headerLen, err);
     if (err) {
         handleError("Error occured reading header length");
-        return 0;
+        return "";
     }
+    // Convert the length into unsigned, and set *len, so we can start processing body
+    // Max is ffffffff or 4294967295
     unsigned length = (headerLen[0] << 24) | (headerLen[1] << 16) | (headerLen[2] << 8) | headerLen[3];
-    std::cout << "Got header length " << length << std::endl;
-    return (size_t)length;
+    ret += std::to_string(length);
+    ret += "]";
+    *len = (size_t)length;
+    return ret;
 }
 
 void TcpConnection::handleError(std::string message = "") {
-    sock_.close();
     log_.Error("An error has occured for: " + remoteIpAddress_ + ", " + message);
     err_ = true;
     ipStats_->RegisterErrors(remoteIpAddress_);
+    sock_.close();
 }
 
 void TcpConnection::ReadPackets() {
-    while (!byeReceived_) {
-        size_t bodyLen = processHeader();
+    while (!byeReceived_) { // Keep reading until header type Goodbye is received
+        size_t bodyLen;
+        std::string header = processHeader(&bodyLen);
         if (err_) break;
-        if (bodyLen) processBody(bodyLen);
+        std::string body = "";
+        if (bodyLen) {
+            body = processBody(bodyLen);
+        }
         if (err_) break;
+        // Finished reading entire TLV blob, print the results as required
+        log_ << "[" << remoteIpAddress_ << ":" << remotePort_ << "] " << header << " [" << body << "]\n";
     }
     if (!err_) {
-        sock_.close();
         ipStats_->RegisterSuccess(remoteIpAddress_);
+        sock_.close();
     }
     active_ = false;
 }
@@ -152,9 +177,8 @@ byeReceived_(false),
 active_(true),
 err_(false)
 {
-    log_.Info("New Connection created for " + remoteIpAddress_);
 }
 
 TcpConnection::~TcpConnection() {
-    log_.Info("Closing connection for " + remoteIpAddress_);
+    // Potentially can print out some interesting stats as well
 }
